b7163bc786d63d0f87a40b6691c2b435a7388e4f
VLR19 <volodymyrovych.popovych@student.ukf.sk>
2018-05-12 02:10:08 +0200

    TRETY commit

---
+++ application/controllers/Zakaznici.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Валерий
+ * Date: 12.05.2018
+ * Time: 1:32
+ */
+
+class Zakaznici
+{
+
+}
---
+++ application/views/zakaznici/view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Валерий
+ * Date: 12.05.2018
+ * Time: 2:06
+ */
